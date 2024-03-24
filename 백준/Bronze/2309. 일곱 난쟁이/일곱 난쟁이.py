69151d92ac8979a3eab07e3ee535800831ebebee
김회일 <106239283+KimHoeil@users.noreply.github.com>
2024-03-24 11:26:04 +0900

    [Bronze I] Title: 일곱 난쟁이, Time: 60 ms, Memory: 31724 KB -BaekjoonHub

---
+++ 백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.py
@@ -0,0 +1,32 @@
+# 일곱 난쟁이 브론즈 1
+
+import sys
+import copy
+
+arr = []
+resultArr = [0] * 9
+visited = [False] * 9
+result = []
+
+for i in range(9):
+    inp = int(input())
+    arr.append(inp)
+
+def dfs(start:int, depth:int):
+    global result
+
+    if depth == 7:
+        if sum(resultArr) == 100:
+            result = copy.deepcopy(resultArr)
+        return
+    
+    for i in range(start,9):
+        resultArr[depth] = arr[i]
+        dfs(i+1, depth+1)
+    
+dfs(0,0)
+result.sort()
+
+for i in result:
+    if i !=0:
+        print(i)
