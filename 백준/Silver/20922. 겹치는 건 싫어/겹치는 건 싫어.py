225f990123ff27109fb96c0e637c01cf11908e44
김회일 <106239283+KimHoeil@users.noreply.github.com>
2024-04-13 11:14:44 +0900

    [Silver I] Title: 겹치는 건 싫어, Time: 252 ms, Memory: 53244 KB -BaekjoonHub

--- 백준/Silver/20922. 겹치는 건 싫어/겹치는 건 싫어.py
+++ 백준/Silver/20922. 겹치는 건 싫어/겹치는 건 싫어.py
@@ -14,14 +14,18 @@ end = 0
 cnt = 0
 maxCnt = 0
 
-for start in range(n):
-     while  end < n and count[sequence[end]] +1 <= k:
+# 투 포인터 알고리즘
+for start in range(n): # start -> n까지 증가
+
+     # end가 수열범위내이고, 특정 원소 개수가 k개 이하이면 개수 증가
+     while  end < n and count[sequence[end]] +1 <= k: 
           cnt +=1
           count[sequence[end]] +=1
           end +=1
           maxCnt = max(maxCnt, cnt)
 
+     # 특정 원소 개수가 k개 초과하면 start증가하고, 특정 원소 카운트 감소
      count[sequence[start]] -=1 
-     cnt -=1
+     cnt -=1 # 부분 수열의 길이도 감소
 
 print(maxCnt)
