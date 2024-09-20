84a23046fff00f35ea5f0623b40386a0e819a072
김회일 <106239283+KimHoeil@users.noreply.github.com>
2024-09-20 15:21:26 +0900

    [Silver I] Title: 단지번호붙이기, Time: 56 ms, Memory: 34088 KB -BaekjoonHub

--- 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.py
+++ 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.py
@@ -1,59 +1,38 @@
-# 단지번호 붙이기 실버 1 bfs
-# 시작시간 38분 start
-# 아이고....이렇게 멍청했다니..ㅠㅠ
-# 왜 처음부터 끝까지 보는 방법을 떠올리지 못햇을가...
-
 import sys
-import copy
-
+from collections import deque
+input = sys.stdin.readline
 n = int(input())
-arr = [[] for i in range(n)]
-d= [(-1,0),(1,0),(0,-1),(0,1)] # 상,하,좌,우 (x,y)
-visited = [[False for j in range(n)] for i in range(n)]
+
+board = [list(map(int, input().strip())) for _ in range(n)]
 ans = []
+def bfs(x,y):
+    dx = [-1,1,0,0]
+    dy = [0,0,-1,1]
 
-def vaild(x:int,y:int)->bool:
-    if x>=0 and x <n and y>=0 and y<n: return True
-    return False
-
-def bfs(x:int, y:int):
-    global dis,arr
-    visited[x][y] = True
-    start = (x,y)
-    cnt = 1
+    q = deque([])
+    q.append((x,y))
+    board[x][y] = 0
     
-    q=[]
-    q.append(start)
-
-    while len(q) != 0:
-        front = q.pop(0)
-
+    cnt =1
+    while q:
+        curx,cury = q.popleft()
         for i in range(4):
-            nx = front[0] + d[i][0]
-            ny = front[1] + d[i][1]
+            nx = curx + dx[i]
+            ny = cury + dy[i]
+            if 0<=nx<n and 0<=ny<n:
+                if board[nx][ny] == 1:
+                    board[nx][ny] = 0
+                    q.append((nx,ny))
+                    cnt +=1
+    ans.append(cnt)
 
-            if vaild(nx,ny) and visited[nx][ny]==False and arr[nx][ny]==1:
-                arr[nx][ny] = 0
-                cnt +=1
-                visited[nx][ny] = True
-                q.append((nx,ny))
-                                
-    return cnt
-
-for i in range(n):
-    ipt = list(input())
-    ipt = list(map(int, ipt))
-    arr[i] = ipt
-
-dis = copy.deepcopy(arr)
-
-dangiCnt = 0
+cnt = 0
 for i in range(n):
     for j in range(n):
-        if arr[i][j] ==1:
-            dangiCnt +=1
-            ans.append(bfs(i,j))
-print(dangiCnt)
+        if board[i][j] == 1:
+            bfs(i,j)
+            cnt+=1
 ans.sort()
-for i in ans:
-    print(i)
+print(cnt)
+for answer in ans:
+    print(answer)
