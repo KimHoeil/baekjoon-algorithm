b7dadf10403d5a2c0276f6bc8cbb1e211b45a462
김회일 <106239283+KimHoeil@users.noreply.github.com>
2024-09-25 13:00:43 +0900

    [Silver III] Title: 계단 오르기, Time: 28 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/2579. 계단 오르기/계단 오르기.py
+++ 백준/Silver/2579. 계단 오르기/계단 오르기.py
@@ -1,29 +1,22 @@
-# https://www.acmicpc.net/problem/2579
-
 import sys
-input= sys.stdin.readline
+input = sys.stdin.readline
 
 n = int(input())
-
-score = [0] * (n+1)
-for i in range(1, n+1):
-    score[i] = int(input())
+score = [0]
+for _ in range(n):
+    score.append(int(input()))
 
 if n == 1:
     print(score[1])
     exit()
 
 dp = [0] * (n+1)
-dp[1] = score[1]
-dp[2] = score[1] + score[2]
+dp[1] = score[1]    
+dp[2] = score[1]+score[2]
 
 for i in range(3, n+1):
-    score1 = dp[i-2] + score[i]
-    score2 = dp[i-3] + score[i-1] + score[i]
-    dp[i] = max(score1, score2)
-    
+    score1 = dp[i-3] + score[i-1] + score[i]
+    score2 = dp[i-2] + score[i]
+    dp[i] = max(score1,score2)
+
 print(dp[n])
-
-
-
-
