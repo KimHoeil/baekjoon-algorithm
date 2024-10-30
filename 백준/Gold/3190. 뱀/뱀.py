c4f9fa3abd9ba6be87244dde8422b36bf23d58b0
김회일 <106239283+KimHoeil@users.noreply.github.com>
2024-10-30 22:05:10 +0900

    [Gold IV] Title: 뱀, Time: 56 ms, Memory: 34116 KB -BaekjoonHub

--- 백준/Gold/3190. 뱀/뱀.py
+++ 백준/Gold/3190. 뱀/뱀.py
@@ -1,97 +1,71 @@
-# 뱀
-
 import sys
 from collections import deque
 
-# sys.stdin = open('input.txt','r')
+input = sys.stdin.readline
 
-# L은 왼쪽으로 90도 회전
-# D는 오른쪽으로 90도 회전
-# 북동남서
-direct = [(-1,0),(0,1),(1,0),(0,-1)]
-
-n = int(sys.stdin.readline())
-k = int(sys.stdin.readline())
-     
+n = int(input())
 board = [[0 for j in range(n)] for i in range(n)]
-for i in range(k):
-     r,c = map(int, sys.stdin.readline().split())
-     
-     board[r-1][c-1] = 1 # 사과가 있다는 표시
-
-l = int(sys.stdin.readline())
-
+k = int(input())
+for _ in range(k):
+    x,y = map(int, input().split())
+    board[x-1][y-1] = 1
+l = int(input())
 info = []
-for i in range(l):
-     info.append(sys.stdin.readline().split()) # 게임정보 배열에 문자열로 저장
-
-# print(board)
-# print(info)
-
-# 문제에서 주어진 시간은 1000초 이하로 해결
+for _ in range(l):
+    info.append(input().split())
 info.append((10001, ''))
 
-# 뱀의 방향 전환
-def turn_snake(direction):
-     global turnIdx
-     # 왼쪽으로 회전
-     if direction == 'L':
-          if turnIdx !=0:
-               turnIdx -=1
-          else:
-               turnIdx = 3
-     # 오른쪽으로 회전
-     else:
-          if turnIdx !=3:
-               turnIdx +=1
-          else:
-               turnIdx = 0
-     return
-
-# 뱀 시작위치 초기화
+dx = [-1,0,1,0]
+dy = [0,1,0,-1]
+turnIndex = 1
+cnt = 0
+x,y = 0,0
 snake = deque()
 snake.append((0,0))
+breaker = False
 
-# 뱀의 기본 방향 설정          
-turnIdx = 1
-cnt = 0 # 게임 진행 시간
-start = 1 # 방향을 바꿀 때 출발 시간
-breaker = False # 반복문 탈출 명령
-x,y = 0,0 # 뱀의 머리 위치
+def turn_snake(direct):
+    global turnIndex
 
-# 시물레이션
+    if direct == 'L':
+        if turnIndex !=0:
+            turnIndex -=1
+        else:
+            turnIndex = 3    
+
+    if direct == 'D':
+        if turnIndex !=3:
+            turnIndex +=1
+        else:
+            turnIndex = 0
+
+
+
 for i in range(len(info)):
-     # time = int(info[0]) # 방향전환 시간
-     # c = info[1]      # 방향전환 정보
-     start = cnt+1
-     for _ in range(start, int(info[i][0]) +1):
-          # 이동할 좌표 설정
-          nx = x + direct[turnIdx][0]      
-          ny = y + direct[turnIdx][1]
-          # 이동할 좌표가 벽 또는 자기자신의 몸과 부딪친다면 반복문 종료
-          if nx<0 or nx>=n or ny<0 or ny>=n or (nx,ny) in snake:
-               cnt+=1
-               breaker = True
-               break
-          # 뱀이 이동할 다음 위치에 사과가 있다면
-          if board[nx][ny] ==1:
-               # 사과 먹기
-               board[nx][ny] =0
-               x,y = nx, ny
-               # 뱀의 위치 표시
-               snake.append((x,y))
-          # 다음 위치에 사과가 없다면
-          else:
-               x,y = nx, ny
-               # 뱀의 위치 표시
-               snake.popleft()
-               snake.append((x,y))
-               # 게임 1초씩 증가
-          cnt +=1
-
-     if breaker == True:
-          break
-     # 뱀 이동후 방향 전환
-     turn_snake(info[i][1])
+    
+    start = cnt+1
+    for _ in range(start, int(info[i][0]) + 1):
+        nx = x + dx[turnIndex]
+        ny = y + dy[turnIndex]
+        
+        if 0<=nx<n and 0<=ny<n and (nx,ny) not in snake:
+            x,y = nx, ny
+            # 사과가 아니면
+            if board[nx][ny] != 1:
+                snake.append((nx,ny))
+                snake.popleft()
+            # 사과이면    
+            else:
+                snake.append((nx,ny))
+                board[nx][ny] = 0
+        else:
+            cnt+=1
+            breaker = True
+            break
+        cnt +=1
+    
+    if breaker == True:
+        break
+    turn_snake(info[i][1])
 
 print(cnt)
